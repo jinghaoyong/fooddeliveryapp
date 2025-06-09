ca4cf0100e4f70465f3413c883de074ee818937a
Sky Yong <jinghao0958@email.com>
2025-06-09 23:17:14 +0800

    stopped at 10mins

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -11,10 +11,47 @@ class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Column(children: [
-            
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 2.5,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Colors.red, Colors.orange],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 3,
+              ),
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: Text(""),
+            ),
+            Column(
+              children: [
+                Image.asset(
+                  "images/logo.png",
+                  fit: BoxFit.cover,
+                  width: MediaQuery.of(context).size.width / 1.5,
+                ),
+              ],
+            ),
           ],
-        )),
+        ),
+      ),
     );
   }
 }
